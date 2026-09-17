a986b54bbc25fe21e47429e30345219d16cab09e
Lucas-Quandt <lucasquandt10@gmail.com>
2026-09-17 11:58:41 -0300

    repositories - getAllProdutos

---
+++ src/server.js
@@ -0,0 +1,9 @@
+require('dotenv').config();
+const app = require('./app');
+
+const PORT = process.env.PORT;
+
+
+app.listen(PORT, ()=>{
+    console.log(`Servidor rodadando ncom sucesso na porta ${PORT}`);
+});
